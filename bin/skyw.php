590064327217d2074f4bccca9aa1891b1969e0c7
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-17 11:08:04 -0700

    Update skyw.php

--- bin/skyw.php
+++ bin/skyw.php
@@ -7,11 +7,17 @@ print "\n\t";
 print "\t\tSKYWEST INSTALLER, v1.0";
 print "\n\t";
 print "\n=============================================================================\n";
-print "\nChecking for updated version of this installer?\n";
-shell_exec("composer install");
 
-print "\nUpdate check completed!...\n";
-print "--------------------------------------------------------------------------------\n\n";
+if(isset($argv[1]) && ($argv[1] == '--update' || $argv[1] == '-u')){
+
+	print "\nChecking for updated version of this installer?\n\n";
+	print "--------------------------------------------------------------------------------\n";
+	shell_exec("composer install --prefer-source");
+	print "\nUpdate check completed!...\n";
+	print "--------------------------------------------------------------------------------\n";
+
+}
+print "\n";
 
 $packages = [
 	"www.skywest.com"  => [
@@ -38,10 +44,11 @@ $handle = fopen ("php://stdin","r");
 print "Are you going to be using 'https' or 'ssh' for this?\n";
 print "(Leave blank or enter 0 for https, or enter 1 for ssh):\n";
 
-print "\n\t>>> ";
+print "\n\tProtocol: >>> ";
 $protocol  = (int)trim(fgets($handle)) == 0? 'https': 'ssh';
 print "\n";
 
+$repo = null;
 $names = array();
 $clones = array();
 
@@ -49,15 +56,15 @@ $index = 1;
 print "The following repositories (installed by this tool) were found in the system!\n";
 foreach ($packages as $name => $package) {
 	$names[$index]  = $name;
-	$clones[$index] = $repo = $package[$protocol];
-	echo "\n\t[$index]  $name - $repo";
-	$index++;	
+	$clones[$index] = $pack = $package[$protocol];
+	echo "\n\t[$index]  $name - $pack";
+	$index++;
 }
 
 print "\n\nWhat package would you like to clone?\n";
 print "(Type number in [n] to select; type 0 or leave blank to abort):\n";
 
-print "\n\t>>> ";
+print "\n\tRepository: >>> ";
 $n  = (int)trim(fgets($handle));
 print "\n";
 
@@ -65,50 +72,69 @@ $count = count($packages);
 
 switch ($n){
 	case 0:
-		exit("You've selected to stop this operation. Exiting...\n\r");
+		exit("You've selected to abort this operation. Aborting!...\n\r");
 		break;
 
 	case ($n <= $count):
 		echo "You've selected the following package:\n\n";
-		echo "   $names[$n]: ".($repo = $clones[$n])."\n\n";
+		echo "\tURL: $names[$n]\n\tGIT: ".($repo = $clones[$n])."\n\n";
 		break;
 
 	case ($n > $count):
-		exit("That package '[$n]' doesn't exist!\n");
+		exit("That package '[$n]' doesn't exist! Aborting!...\n");
 		break;
-	
+
 	default:
-		echo "Okay, nothing will be installed. ABORTING...!\n";
+		echo "Okay, nothing will be installed. Aborting!...\n";
 	    exit;
 		break;
+}
+if(empty($repo)){
+	exit("Sorry, you didn't select any repository! Aborting!...");
 }
 print "Where would you like to clone this repository to (relative to this folder)?: \n";
 print "\tType './' for this directory or '../' for parent of this directory to startn\n";
 print "\tThen type directory name you want to create. e.g. '../jumanji', or './jumanji'\n";
+print "\tOr leave it blank to use the default name of the repository for installation'\n";
 
-print "\n\t>>> ";
+print "\n\tLocation: >>> ";
 $dir = trim(fgets($handle));
 print "\n";
 
-$total = count(glob("$dir/*"));
+if(empty($dir)){
+	$parts = explode("/", $repo);
+	$dir   = array_pop($parts);
+	$dir   = './'.str_replace(".git", "", $dir);
+}
+else{
+	$total = count(glob("$dir/*"));
+	$exists = "";
+	$notEmpty = " and, it is not empty!";
 
-$exists = ""; 
-$notEmpty = " and, it is not empty!";
-if(file_exists($dir) || is_dir($dir)) $exists .= "This directory already exists!";
-if($total > 0) $exists .= $notEmpty;
-if($exists != "" && strpos($exists, $notEmpty) !== false)
-	exit("$exists\nPlease you a different directory name!\n\r");
+	if(file_exists($dir) || is_dir($dir))
+		$exists .= "This directory already exists!";
+
+	if($total > 0)
+		$exists .= $notEmpty;
+
+	if($exists != "" && strpos($exists, $notEmpty) !== false)
+		exit("$exists\nPlease you a different directory name!\n\Aborting...\r");
+}
+
+print "Your repository will be cloned in:\n\n \t$dir\n\n";
 
 print "Which branch are you going to clone into $dir?: \n";
-print "(Leave blank for the default branch, usually master, or type the name of the branch, e.g. '1.0-x')\n";
+print "\tType the name of the branch, (e.g. '3.6.4, or 1.0-x')\n";
+print "\tOr, leave blank for the default branch, (usually master)\n";
 
-print "\n\t>>> ";
-$branch = trim(fgets($handle)) ?? 'default';
+print "\n\tBranch: >>> ";
+$branch = trim(fgets($handle));
 print "\n";
- 
-print "Your $branch branch will be created in '$dir. Continue? ['Y' or 'N']: \n";
 
-print "\n\t>>> ";
+$branch .= empty($branch)? 'default branch': ' branch';
+print "Your $branch will be created in '$dir. Continue? ['Y' or 'N']: \n";
+
+print "\n\tConfirm: >>> ";;
 $confirm = trim(fgets($handle));
 print "\n";
 
@@ -116,9 +142,6 @@ if(strtoupper($confirm) != 'Y') exit("Aborting...\n\r");
 if($branch != 'default') $branch = "-b $branch ";
 
 fclose($handle);
-
-// if(!is_dir($dir)) mkdir($dir, 0775, true); 
-// $dir = realpath($dir);
 
 shell_exec("git clone $branch$repo $dir && cd $dir && composer update");
 echo "\n\nThank you, finishing...\n\n\r";
