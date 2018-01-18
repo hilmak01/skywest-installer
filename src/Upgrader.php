e6ef0a7e12a8fb6d9400150a4359d86639461956
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-17 22:22:12 -0700

    Create Upgrader.php

---
+++ src/Upgrader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SKYW;
+
+class Upgrader
+{
+  public function __construct()
+  {
+    $this->run();
+  }
+  
+  public function run()
+  {
+    print "You are about to start upgrading code in this folder ".realpath('./')."\n";
+    
+    $this->checkForModules();
+  }
+  
+  public function checkForModules($DIR)
+  {
+    $Modules = [];
+    $dirs = glob("$DIR/*/composer.json");
+    
+    foreach($dirs as $dir){
+      if(is_dir($dir){
+        $this->checkForModules($dir);
+        continue;
+      }
+      $Modules[] = [basename(dirname($dir)) = $dir];
+    }
+    if(count($Modules) > 0){
+      $this->reorganizeModules($Modules);
+    }
+  }
+  
+  public function reorganizeModules($Modules)
+  {
+    $extras = [];
+    foreach($Modules as $name => $module){
+      $extras['installer-paths']['vendor/$name'] = [
+      
+      ];
+    }
+  }
+}
